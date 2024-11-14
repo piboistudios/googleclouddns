e18d528a21aabd8989e8ff4b3230881596d484b6
topboy <topboy@pop-os.localdomain>
2024-11-14 14:32:52 -0600

    just dont patch, pl0x show me the error PL0X

--- client-post.go
+++ client-post.go
@@ -33,6 +33,7 @@ func (p *Provider) postCloudDNSRecord(ctx context.Context, zone string, recordsT
 	rrs.Rrdatas = recordsToSend.prepValuesForCloudDNS()
 	googleRecord, err := p.service.ResourceRecordSets.Create(p.Project, gcdZone, &rrs).Context(ctx).Do()
 	if err != nil {
+		return nil, err
 		if gErr, ok := err.(*googleapi.Error); !ok || gErr.Code != 409 {
 			return nil, err
 		}
