70f6c44b2cdea0d8b59f14775e1c7b46f680c5ef
topboy <topboy@pop-os.localdomain>
2024-11-14 12:33:05 -0600

    normalize zone in api calls

--- util.go
+++ util.go
@@ -87,3 +87,10 @@ func convertToLibDNS(googleRecord *dns.ResourceRecordSet, zone string) libdnsRec
 	}
 	return records
 }
+
+func normalizeZone(zone string) string {
+	if zone[len(zone)-1:len(zone)] === '.' {
+		return zone
+	}
+	return zone + "."
+}
