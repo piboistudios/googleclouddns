4905e0769589ba2294f4dc2376aa15213015ff98
topboy <topboy@pop-os.localdomain>
2024-11-14 14:01:38 -0600

    pls lob me one

--- client-delete.go
+++ client-delete.go
@@ -15,7 +15,7 @@ func (p *Provider) deleteCloudDNSRecord(ctx context.Context, zone, name, recordT
 	}
 
 	fullName := libdns.AbsoluteName(name, zone)
-	fullName = normalizeHost(fullName) + "test"
+	fullName = normalizeHost(fullName)
 	gcdZone, err := p.getCloudDNSZone(zone)
 	if err != nil {
 		return nil, err
